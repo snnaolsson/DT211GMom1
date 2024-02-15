c4e9065901a0970db3627d0b05e0693cbf58d93b
Sanna Olsson <saol1901@student.miun.se>
2024-02-15 13:29:08 +0100

    functions for "sort". Updated menus on all pages.

--- src/js/ramschema.js
+++ src/js/ramschema.js
@@ -1,10 +1,21 @@
-const scheduleEl = document.getElementById("schedule-list");
-window.onload = init();
+const scheduleEl = document.getElementById("schedule-list"); //Hämtar schedule-list globalt för att kunna använda i flera funktioner.
+const searchbar = document.getElementById("searchbar"); //Hämtar sökrutan
+searchbar.addEventListener("input", searchSchedule); //Lägger till en händelselyssnare till sökrutan som vid input kör funktionen searchSchedule
+const kurskod = document.getElementById("kurskod");
+kurskod.addEventListener("click", sortByCode);
+const kursnamn = document.getElementById("kursnamn");
+kursnamn.addEventListener("click", sortByName);
+const progression = document.getElementById("progression");
+progression.addEventListener("click", sortByProgression);
 
+window.onload = init(); //När fönstret laddas ska funktionen init köras.
+
+//I funktionen init körs loadschedule.
 async function init() {
   loadSchedule();
 }
 
+//Funktion som hämtar json-data och kör funktionen displaySchedule
 async function loadSchedule() {
   try {
     const response = await fetch(
@@ -18,7 +29,9 @@ async function loadSchedule() {
   }
 }
 
+//Funktion som loopar igenom datan och skriver ut den till gränssnittet
 function displaySchedule(courseInfo) {
+  scheduleEl.innerHTML = "";
   courseInfo.forEach((course) => {
     scheduleEl.innerHTML += `
     <tr>
@@ -29,16 +42,14 @@ function displaySchedule(courseInfo) {
   });
 }
 
-const searchbar = document.getElementById("searchbar");
-searchbar.addEventListener("input", searchSchedule);
-
-//funktion som filtrerar datan från API utifrån input i sökrutan och skriver ut de kurserna i DOM.
+//funktion som filtrerar datan utifrån input i sökrutan och skriver ut de kurserna i gränssnittet.
 async function searchSchedule() {
   try {
     const response = await fetch(
       "https://webbutveckling.miun.se/files/ramschema_ht23.json"
     );
     const courseInfo = await response.json();
+    scheduleEl.innerHTML = "";
 
     /*skapar en array med de filtrerade resultaten. Filtrerar utifrån att kursnamnet ska innehålla det som skrivits i sökrutan eller att kurskoden ska innehålla det.
     Sätter båda till toLowerCase då funktionen är skiftlägeskänslig. */
@@ -48,10 +59,8 @@ async function searchSchedule() {
         info.code.toLowerCase().includes(`${searchbar.value}`)
       );
     });
-    //Återställer tidigare innehåll så att inte tidigare resultat också finns kvar
-    scheduleEl.innerHTML = "";
 
-    /*Loopar igenom och skriver ut den filtrerade arrayen till DOM */
+    /*Loopar igenom och skriver ut den filtrerade arrayen till gränssnittet */
     filteredSchedule.forEach((course) => {
       scheduleEl.innerHTML += `
         <tr>
@@ -62,5 +71,76 @@ async function searchSchedule() {
     });
   } catch (error) {
     console.log("Något gick fel:", error);
+  }
+}
+
+//Sorterar på kurskod
+async function sortByCode() {
+  try {
+    const response = await fetch(
+      "https://webbutveckling.miun.se/files/ramschema_ht23.json"
+    );
+    const courseInfo = await response.json();
+    scheduleEl.innerHTML = "";
+    courseInfo.sort((a, b) => {
+      return a.code > b.code ? 1 : -1;
+    });
+    courseInfo.forEach((course) => {
+      scheduleEl.innerHTML += `
+          <tr>
+            <td>${course.code}</td>
+            <td>${course.coursename}</td>
+            <td>${course.progression}</td>
+          </tr>`;
+    });
+  } catch (error) {
+    console.log("Något gick fel");
+  }
+}
+
+//sorterar på kursnamn
+async function sortByName() {
+  try {
+    const response = await fetch(
+      "https://webbutveckling.miun.se/files/ramschema_ht23.json"
+    );
+    const courseInfo = await response.json();
+    scheduleEl.innerHTML = "";
+    courseInfo.sort((a, b) => {
+      return a.coursename > b.coursename ? 1 : -1;
+    });
+    courseInfo.forEach((course) => {
+      scheduleEl.innerHTML += `
+            <tr>
+              <td>${course.code}</td>
+              <td>${course.coursename}</td>
+              <td>${course.progression}</td>
+            </tr>`;
+    });
+  } catch (error) {
+    console.log("Något gick fel");
+  }
+}
+//sorterar på progression
+async function sortByProgression() {
+  try {
+    const response = await fetch(
+      "https://webbutveckling.miun.se/files/ramschema_ht23.json"
+    );
+    const courseInfo = await response.json();
+    scheduleEl.innerHTML = "";
+    courseInfo.sort((a, b) => {
+      return a.progression > b.progression ? 1 : -1;
+    });
+    courseInfo.forEach((course) => {
+      scheduleEl.innerHTML += `
+              <tr>
+                <td>${course.code}</td>
+                <td>${course.coursename}</td>
+                <td>${course.progression}</td>
+              </tr>`;
+    });
+  } catch (error) {
+    console.log("Något gick fel");
   }
 }
